7fd116063d6200e1ef039342ce8366e1d4be346f
pierre <pmonier99@laposte.net>
2021-11-15 15:41:29 +0100

    refacto(library): simplify LibraryList widget

--- lib/library/widget/library_list.dart
+++ lib/library/widget/library_list.dart
@@ -9,31 +9,40 @@ import 'package:get_it/get_it.dart';
 class LibraryList extends StatelessWidget {
   const LibraryList({Key? key}) : super(key: key);
 
+  int _getItemCount(LibraryState currentState) {
+    if (currentState is ShowMangas) {
+      return GetIt.I.get<MangaRepository>().mangaList.length;
+    } else if (currentState is ShowChapters) {
+      return currentState.manga.chapters.length;
+    } else {
+      return 0;
+    }
+  }
+
+  Widget _getItem(LibraryState currentState, int index) {
+    if (currentState is ShowMangas) {
+      return LibraryListMangaItem(
+          GetIt.I.get<MangaRepository>().mangaList[index]);
+    } else if (currentState is ShowChapters) {
+      return LibraryListChapterItem(currentState.manga.chapters[index]);
+    } else {
+      return Text("TODO: error widget");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LibraryBloc, LibraryState>(builder: (context, state) {
-      if (state is ShowMangas) {
+      if (state is ShowMangas || state is ShowChapters) {
         return GridView.builder(
             gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
                 maxCrossAxisExtent: 200,
                 childAspectRatio: 1,
                 crossAxisSpacing: 10,
                 mainAxisSpacing: 10),
-            itemCount: GetIt.I.get<MangaRepository>().mangaList.length,
+            itemCount: _getItemCount(state),
             itemBuilder: (BuildContext ctx, index) {
-              return LibraryListMangaItem(
-                  GetIt.I.get<MangaRepository>().mangaList[index]);
-            });
-      } else if (state is ShowChapters) {
-        return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                childAspectRatio: 1,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10),
-            itemCount: state.manga.chapters.length,
-            itemBuilder: (BuildContext ctx, index) {
-              return LibraryListChapterItem(state.manga.chapters[index]);
+              return _getItem(state, index);
             });
       } else {
         return Text("TODO: error widget");
