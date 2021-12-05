9599da2fb149a0c77f4a65e801e8e280b196f8ad
pierre <pmonier99@laposte.net>
2021-12-05 16:15:48 +0100

    wip(backup): copying manga files

--- lib/service/backup_service.dart
+++ lib/service/backup_service.dart
@@ -3,29 +3,34 @@ import 'dart:io';
 import 'package:cross_reader/model/chapter.dart';
 import 'package:cross_reader/model/manga.dart';
 import 'package:cross_reader/repository/chapter_repository.dart';
+import 'package:file/local.dart';
 import 'package:get_it/get_it.dart';
+import 'package:file/file.dart' show FileSystem;
 
 class BackupService {
   final Directory _cacheDirectory;
-  final bool shouldMock;
-  const BackupService(this._cacheDirectory, {this.shouldMock = false});
+  final FileSystem fileSystem;
+  const BackupService(this._cacheDirectory,
+      {this.fileSystem = const LocalFileSystem()});
 
   static const backupDirName = 'backup/';
   static const mangasJsonFileName = 'mangas.json';
 
   Future<BackupFiles> getBackupFiles() async {
-    final backupDirectory = Directory('${_cacheDirectory.path}/$backupDirName');
-    final mangasJsonFile = File('${backupDirectory.path}/$mangasJsonFileName');
+    final backupDirectory =
+        fileSystem.directory('${_cacheDirectory.path}/$backupDirName');
+    final mangasJsonFile =
+        fileSystem.file('${backupDirectory.path}/$mangasJsonFileName');
 
-    if (!shouldMock) {
-      await backupDirectory.create();
-      await mangasJsonFile.create();
-    }
+    // if (!shouldMock) {
+    await backupDirectory.create(recursive: true);
+    await mangasJsonFile.create(recursive: true);
+    // }
 
     return BackupFiles(backupDirectory, mangasJsonFile);
   }
 
-  List<Manga> transformImagesPath(BackupFiles backupFiles, List<Manga> mangas) {
+  List<Manga> createBackupMangas(BackupFiles backupFiles, List<Manga> mangas) {
     return mangas.map((manga) {
       final newDirPath = '${backupFiles.backupDirectory.path}/${manga.name}/';
       final List<Chapter> chapters = GetIt.I
@@ -34,6 +39,33 @@ class BackupService {
 
       return Manga(manga.name, chapters);
     }).toList();
+  }
+
+  Future<bool> copyMangasFiles(List<Manga> mangas) async {
+    try {
+      final backupImagesPath = [];
+      final onDeviceImagesPath = [];
+
+      if (backupImagesPath.length == onDeviceImagesPath.length) {
+        // length of arrays
+        final length = backupImagesPath.length;
+
+        for (var i = 0; i < length; i++) {
+          final onDeviceFile = fileSystem.file(onDeviceImagesPath[i]);
+          final file = fileSystem.file(backupImagesPath[i]);
+
+          await file.create(recursive: true);
+          await onDeviceFile.copy(file.path);
+        }
+
+        return true;
+      } else {
+        throw Exception(
+            'Backup files and on device files doesn\'t have the same size');
+      }
+    } catch (e) {
+      return false;
+    }
   }
 }
 
--- test/service/backup_service_test.dart
+++ test/service/backup_service_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:cross_reader/repository/chapter_repository.dart';
 import 'package:cross_reader/service/backup_service.dart';
 import 'package:cross_reader/service/box_service.dart';
+import 'package:file/memory.dart';
 import 'package:get_it/get_it.dart';
 import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -10,9 +11,9 @@ import 'package:mocktail/mocktail.dart';
 import '../utils/mock_class.dart';
 import '../utils/mock_data.dart';
 
-const cacheDirPath = '/tmpPath/';
-const backupDirPath = cacheDirPath + '/backup/';
-const mangasJsonFile = backupDirPath + 'mangas.json';
+final cacheDirPath = Directory.current.path;
+final backupDirPath = cacheDirPath + '/backup/';
+final mangasJsonFile = backupDirPath + 'mangas.json';
 
 void main() {
   setUpAll(() {
@@ -25,7 +26,7 @@ void main() {
   test('It should create backup dir with a file named mangas.json inside',
       () async {
     final backupService =
-        BackupService(Directory(cacheDirPath), shouldMock: true);
+        BackupService(Directory(cacheDirPath), fileSystem: MemoryFileSystem());
     final backupFiles = await backupService.getBackupFiles();
 
     final backupDirPath = '$cacheDirPath/${BackupService.backupDirName}';
@@ -34,16 +35,17 @@ void main() {
 
     expect(backupFiles.backupDirectory.path, backupDirPath);
     expect(backupFiles.mangasJsonFile.path, mangasJsonFilePath);
+    expect(backupFiles.backupDirectory.existsSync(), true);
+    expect(backupFiles.mangasJsonFile.existsSync(), true);
   });
 
   test('It should transform all images path to match the backup directory',
       () async {
     final mangas = await GetIt.I.get<BoxService>().getAllMangas();
     final backupService =
-        BackupService(Directory(cacheDirPath), shouldMock: true);
+        BackupService(Directory(cacheDirPath), fileSystem: MemoryFileSystem());
     final backupFiles = await backupService.getBackupFiles();
-    final mutatedMangas =
-        backupService.transformImagesPath(backupFiles, mangas);
+    final mutatedMangas = backupService.createBackupMangas(backupFiles, mangas);
 
     final newDirPath = '$backupDirPath/${mangas[0].name}/';
 
@@ -54,4 +56,21 @@ void main() {
     expect(mutatedMangas[0].chapters[0].imagesPath[0],
         '$newDirPath/${mangas[0].chapters[0].name}/${mangas[0].chapters[0].imagesPath[0]}');
   }, skip: true);
+
+  test('It should copy all mangas images to backupDir', () async {
+    final mangas = await GetIt.I.get<BoxService>().getAllMangas();
+    final backupService =
+        BackupService(Directory(cacheDirPath), fileSystem: MemoryFileSystem());
+    final backupFiles = await backupService.getBackupFiles();
+    final backupMangas = backupService.createBackupMangas(backupFiles, mangas);
+    await backupService.copyMangasFiles(backupMangas);
+
+    final mangaDir = Directory('$backupDirPath/${backupMangas[0].name}');
+    final chapterDir = Directory('$backupDirPath/${backupMangas[0].name}');
+    final imageFile = File(backupMangas[0].chapters[0].imagesPath[0]);
+
+    expect(mangaDir.existsSync(), true);
+    expect(chapterDir.existsSync(), true);
+    expect(imageFile.existsSync(), true);
+  });
 }
